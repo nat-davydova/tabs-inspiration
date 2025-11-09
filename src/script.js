24445ea7bf28ad50145e6fe9b7c833031d14e273
Natalia <natti.davydova@gmail.com>
2025-11-09 17:24:18 +0300

    effect #1: add effect init function

--- src/script.js
+++ src/script.js
@@ -1,9 +1,22 @@
 const DOM = {
-  tabs_1: "tabs--1",
+  tabs: "tabs",
   tabs_list: "tabs__list",
   tabs_btn: ".tabs__btn",
-  is_active: "is-active"
+  is_active: "is-active",
+  tabsDataAttr: "data-tabs-effect"
 };
+
+// ****** TABS EFFECTS INITS ******
+setTabs({
+  tabsContainerClassname: ".tabs-effect-1"
+});
+
+function setTabs({ tabsContainerClassname }) {
+  const tabsContainer = document.querySelector(tabsContainerClassname);
+  const effectNumber = tabsContainer.getAttribute(DOM.tabsDataAttr);
+
+  tabsContainer.classList.add(`${DOM.tabs}--${effectNumber}`);
+}
 
 // ****** UTILS ******
 function getActiveElementFromTab(tabsClassname) {
